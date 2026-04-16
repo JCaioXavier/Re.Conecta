04df91bb383355d3fd569a2ec706e1f5bf290a76
José Caio <caio_xavier@outlook.com>
2026-04-15 21:51:59 -0300

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,5 +2,13 @@ const AuthController = {
     async login(req, res) {
         const { email, password } = req.body;
         return res.json({ token: "token_gerado", user: "Ryan Austin" });
+    },
+
+    // Função adicionada
+    async forgotPassword(req, res) {
+        const { email } = req.body;
+        // Lógica para enviar email de recuperação
+        return res.json({ message: "Se o email existir, enviámos um link de recuperação." });
     }
 };
+module.exports = AuthController;
